57c8bc0fb24f580e31b2125759e1328978a9e7a8
unknown <danilomunhe@gmail.com>
2022-01-27 20:41:33 -0300

    Alterando cor do texto

--- JS/calculo.js
+++ JS/calculo.js
@@ -10,18 +10,37 @@ function calcularImc(altura, peso){
 }
 function classificarImc(imc){
     let indice = ''
+    let texto = document.getElementById('texto')
     if(imc <= 18.5){
         indice = 'você está abaixo de peso'
+        texto.textContent= ""
+        texto.classList.remove('vermelho')
+        texto.classList.remove('verde')
     }else if(imc < 25){
         indice = `seu peso está normal.`
+        texto.textContent= "Parabéns"
+        texto.classList.remove('vermelho')
+        texto.classList.add('verde')
     }else if(imc < 30){
         indice = 'você está com sobrepeso'
+        texto.textContent= ""
+        texto.classList.remove('vermelho')
+        texto.classList.remove('verde')
     }else if(imc < 35){
         indice = 'você está com obesidade I'
+        texto.textContent= ""
+        texto.classList.remove('vermelho')
+        texto.classList.remove('verde')
     }else if(imc < 40){
         indice = 'você está com obesidade II'
+        texto.textContent= ""
+        texto.classList.remove('vermelho')
+        texto.classList.remove('verde')
     }else{
         indice = 'você está com obesidade mórbita'
+        texto.textContent= "Cuidado!!"
+        texto.classList.remove('verde')
+        texto.classList.add('vermelho')
     }
     return indice
 }
@@ -30,6 +49,7 @@ function mostrarResultado(){
     const altura = document.getElementById('altura').value
     const peso = document.getElementById('peso').value
     const resultado = document.getElementById('resultado')
+    
  
     if(camposValidos()){
         const imc = calcularImc(altura, peso)
