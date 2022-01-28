2ffcc9d448fe381f36b3dba5400f617650609474
unknown <danilomunhe@gmail.com>
2022-01-28 00:42:12 -0300

    Trocando de cor utilizando o JavaScript para criar a tag e fazer alterações na mesma

--- JS/calculo.js
+++ JS/calculo.js
@@ -10,51 +10,55 @@ function calcularImc(altura, peso){
 }
 function classificarImc(imc){
     let indice = ''
-    let texto = document.getElementById('texto')
     if(imc <= 18.5){
         indice = 'você está abaixo de peso'
-        texto.textContent= ""
-        texto.classList.remove('vermelho')
-        texto.classList.remove('verde')
     }else if(imc < 25){
         indice = `seu peso está normal.`
-        texto.textContent= "Parabéns"
-        texto.classList.remove('vermelho')
-        texto.classList.add('verde')
     }else if(imc < 30){
         indice = 'você está com sobrepeso'
-        texto.textContent= ""
-        texto.classList.remove('vermelho')
-        texto.classList.remove('verde')
     }else if(imc < 35){
         indice = 'você está com obesidade I'
-        texto.textContent= ""
-        texto.classList.remove('vermelho')
-        texto.classList.remove('verde')
     }else if(imc < 40){
         indice = 'você está com obesidade II'
-        texto.textContent= ""
-        texto.classList.remove('vermelho')
-        texto.classList.remove('verde')
     }else{
         indice = 'você está com obesidade mórbita'
-        texto.textContent= "Cuidado!!"
-        texto.classList.remove('verde')
-        texto.classList.add('vermelho')
     }
     return indice
+}
+function mensagens(imc){
+    let mensagem = document.createElement('p')
+    let texto
+    if(imc > 18.5 && imc < 25){
+        texto = document.createTextNode("Parabéns!!!")
+        mensagem.appendChild(texto)
+        mensagem.classList.remove('vermelho')
+        mensagem.classList.add('verde')
+    }else if(imc > 40){
+        texto = document.createTextNode("Cuidado!!")
+        mensagem.appendChild(texto)
+        mensagem.classList.remove('verde')
+        mensagem.classList.add('vermelho')
+    }else{
+        texto = document.createTextNode("")
+        mensagem.appendChild(texto)
+        mensagem.classList.remove('verde')
+        mensagem.classList.add('vermelho')
+    }
+    return mensagem
 }
 function mostrarResultado(){
     const nome = document.getElementById('nome').value
     const altura = document.getElementById('altura').value
     const peso = document.getElementById('peso').value
-    const resultado = document.getElementById('resultado')
+    const resultado = document.getElementById('result')
     
  
     if(camposValidos()){
         const imc = calcularImc(altura, peso)
         const indice = classificarImc(imc)
-        resultado.textContent = `${nome}, seu imc é: ${imc.toFixed(2)} ${indice}`
+        const alerta = mensagens(imc)
+        resultado.textContent = `${nome}, seu imc é: ${imc.toFixed(2)} ${indice} `
+        resultado.appendChild(alerta)
     }else{
         resultado.textContent = "Preencha todos os campos"
     }
